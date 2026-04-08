1b452d0f96247b04930f6ad8e8d50cb91f6a9d68
Julianxalessio <julian.lombardo@outlook.com>
2026-04-08 20:48:09 +0200

    now working

--- getData.js
+++ getData.js
@@ -1,13 +1,3 @@
-fetch('https://ipapi.co/json/')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        dataNew = data;
-    });
-console.log(dataNew);
-
 let platformData = {
     name: platform.name,
     version: platform.version,
@@ -15,28 +5,38 @@ let platformData = {
     os: platform.os,
     description: platform.description
 };
-
+let templateParams = {};
 let dateNow = new Date();
 
 emailjs.init("zM45sD-mfpvGvAZ_v"); // Ersetze mit deinem Public Key von emailjs.com
-const EMAILJS_SERVICE_ID = "service_sas30ng";
-const EMAILJS_TEMPLATE_ID = "template_kriko7m";
+let EMAILJS_SERVICE_ID = "service_sas30ng";
+let EMAILJS_TEMPLATE_ID_MAIN = "template_kriko7m";
 
-const templateParams = {
-    ip: dataNew.ip,
-    city: dataNew.city,
-    region: dataNew.region,
-    country: dataNew.country,
-    country_name: dataNew.country_name,
-    platform_name: platformData.name,
-    platform_version: platformData.version,
-    platform_layout: platformData.layout,
-    platform_os: platformData.os.toString(),
-    platform_description: platformData.description,
-    date: dateNow.toString()
-};
-try {
-    const response = emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, templateParams);
-} catch (error) {
-    console.error("EmailJS error:", error);
-}
+fetch('https://ipapi.co/json/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        templateParams = {
+            ip: data.ip,
+            city: data.city,
+            region: data.region,
+            country: data.country,
+            country_name: data.country_name,
+            platform_name: platformData.name,
+            platform_version: platformData.version,
+            platform_layout: platformData.layout,
+            platform_os: platformData.os.toString(),
+            platform_description: platformData.description,
+            date: dateNow.toString()
+        };
+        try {
+            const response = emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID_MAIN, templateParams);
+        } catch (error) {
+            console.error("EmailJS error:", error);
+        }
+    })
+    .catch(function (error) {
+        console.error('Error fetching IP data:', error);
+    });
